5d29d073455a391fede45e1b0737c4376ed86a59
Pankaj M. Patidar <39170113+imPatidar@users.noreply.github.com>
2018-09-07 00:54:04 +0530

    Add files via upload

---
+++ src/campus_placement_mgt/Campus_Placement_mgt.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package campus_placement_mgt;
+
+/**
+ *
+ * @author Anjali
+ */
+public class Campus_Placement_mgt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login_Form m=new Login_Form();
+        m.setVisible(true);
+    }
+    
+}
